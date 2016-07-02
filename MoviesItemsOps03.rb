502146746be9f091289ac1c70bb6481ec74a353c
Martín Trejo Chávez <chilli.coder@gmail.com>
2016-07-01 23:24:33 -0500

    Initial commit

---
+++ MoviesItemsOps03.rb
@@ -0,0 +1,38 @@
+require "aws-sdk-core"
+
+Aws.config.update({
+  region: "us-west-2",
+  endpoint: "http://localhost:8000"
+})
+
+dynamodb = Aws::DynamoDB::Client.new
+
+tableName = "Movies"
+
+year = 2015
+title = "The Big New Movie"
+
+params = {
+  table_name: "Movies",
+  key: {
+    year: year,
+    title: title
+  },
+  update_expression: "set info.rating = :r, info.plot = :p, info.actors = :a",
+  expression_attribute_values: {
+    ":r" => 5.5,
+    ":p" => "Everything happens all at once",
+    ":a" => ["Larry","Moe","Curly"]
+  },
+  return_values: "UPDATED_NEW"
+}
+
+begin
+  result = dynamodb.update_item(params)
+  puts "Item updated: #{year} - #{title}"
+
+rescue Aws::DynamoDB::Errors::ServiceError => error
+  puts "Unable to add item:"
+  puts "#{error.message}"
+
+end
